45fc6839400724830498150a3aa86de7282ffdfa
Amanda Kievet <akievet@gmail.com>
2017-05-18 16:35:53 -0400

    Returns a random image each time

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function insertRandomImages() {
   var container = document.getElementById('photo-tiles');
   for (var i = 0; i < 10; i++) {
-    var image = document.createElement('img')
-    image.src = 'http://assets.ibex.com.s3.amazonaws.com/images/ultimatecampout/Ultimate_Campout_Ibex_00' + i + '.jpg';
+    var rand = getRandomInt(1,60).toString();
+    var imageNum = rand.length == 1 ? '0' + rand : rand;
+    var image = document.createElement('img');
+    image.src = 'http://assets.ibex.com.s3.amazonaws.com/images/ultimatecampout/Ultimate_Campout_Ibex_0' + imageNum + '.jpg';
     container.appendChild(image);
   }
 }
