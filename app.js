a431b8cbf06bc835fab5acfc206ae9a7ba8d9833
Amanda Kievet <akievet@gmail.com>
2017-05-19 11:23:26 -0400

    Adds boostrap and some styles

--- app.js
+++ app.js
@@ -14,10 +14,17 @@ function insertRandomImages() {
   for (var i = 0; i < imageNums.length; i++) {
     var imageNum = imageNums[i].toString();
     var formatted = imageNum.length == 1 ? '0' + imageNum : imageNum;
+    var div = document.createElement('div');
+    div.className = 'col-xs-6 col-md-3';
+    var innerDiv = document.createElement('div');
+    innerDiv.className = 'thumbnail';
+
     var image = document.createElement('img');
     image.src = 'http://assets.ibex.com.s3.amazonaws.com/images/ultimatecampout/Ultimate_Campout_Ibex_0' + formatted + '.jpg';
     image.className = 'photo-tile';
-    container.appendChild(image);
+    innerDiv.appendChild(image);
+    div.appendChild(innerDiv);
+    container.appendChild(div);
   }
 }
 
